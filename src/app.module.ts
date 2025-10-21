079d0a9a68a04ddc846475df5fea62be072b2d3c
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 15:18:47 -0300

    Arquitetura do Modulo Charge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './modules/customer/customer.module';
 import { PrismaService } from './infra/database/prisma.service';
+import { ChargeModule } from './modules/charge/charge.module';
 
 @Module({
   imports: [
     CustomerModule,
+    ChargeModule,
   ],
   controllers: [],
   providers: [
---
+++ src/modules/charge/charge.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from "@nestjs/common";
+
+
+@Controller("charges")
+export class ChargeController {}
