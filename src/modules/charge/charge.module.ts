97bb0a5f0053771cdf33b4b505bbfa183cf2b00f
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 16:47:38 -0300

    Importações no ChargeModule

--- src/modules/charge/charge.module.ts
+++ src/modules/charge/charge.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
+import { ChargeController } from "./charge.controller";
+import { PrismaService } from "src/infra/database/prisma.service";
+import { ChargeRepository } from "./repositories/charge.repository";
+import { ChargePrismaRepository } from "./repositories/prisma/charge.prisma.repository";
 
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [ChargeController],
+    providers: [
+        PrismaService,
+        {
+            provide: ChargeRepository,
+            useClass: ChargePrismaRepository,
+        }
+    ],
 })
 
 export class ChargeModule {}
