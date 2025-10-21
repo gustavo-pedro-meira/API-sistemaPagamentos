095b1f8f9681417fba870ba196f2f86dd2c10af0
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 11:12:08 -0300

    CustomerRepository definido

--- src/modules/customer/repositories/customer.repository.ts
+++ src/modules/customer/repositories/customer.repository.ts
@@ -1,5 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { CreatedCustomerDto, CustomerCreatedDto } from "../dto/created-customer.dto";
 
 
 @Injectable()
-export abstract class CustomerRepository {}
+export abstract class CustomerRepository {
+    abstract findAll(): Promise<CustomerCreatedDto[] | null>;
+    abstract findOne(id: string): Promise<CustomerCreatedDto | null>;
+    abstract deleteById(id: string): Promise<CustomerCreatedDto | null>;
+    abstract create(createdCustomerDto: CreatedCustomerDto): Promise<CustomerCreatedDto | null>;
+}
