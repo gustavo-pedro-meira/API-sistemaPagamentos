9c1f649b160b28e562af263dd9187245eadbe979
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 11:51:32 -0300

    Configuração do ZOD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './modules/customer/customer.module';
+import { PrismaService } from './infra/database/prisma.service';
 
 @Module({
   imports: [
     CustomerModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    PrismaService,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodAny } from 'zod';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ZodValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
