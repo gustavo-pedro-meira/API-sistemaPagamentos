b9438b4c987cfe6d69bf12b29e3d63e330161191
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 11:23:18 -0300

    Metodos no Prisma Repository

---
+++ src/modules/customer/repositories/prisma/customer.prisma.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { CustomerRepository } from "../customer.repository";
+import { PrismaService } from "src/infra/database/prisma.service";
+import { CustomerCreatedDto, CreatedCustomerDto } from "../../dto/created-customer.dto";
+
+
+@Injectable()
+export class CustomerPrismaRepository implements CustomerRepository {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async findAll(): Promise<CustomerCreatedDto[] | null> {
+        return await this.prismaService.customer.findMany();
+    }
+
+    async findOne(id: string): Promise<CustomerCreatedDto | null> {
+        return await this.prismaService.customer.findUnique({
+            where: { id }
+        });
+    }
+    async deleteById(id: string): Promise<CustomerCreatedDto | null> {
+        return await this.prismaService.customer.delete({
+            where: { id }
+        })
+    }
+    async create(createdCustomerDto: CreatedCustomerDto): Promise<CustomerCreatedDto | null> {
+        return await this.prismaService.customer.create({
+            data: { ...createdCustomerDto }
+        })
+    }
+}
