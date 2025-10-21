30b5327b3d3d26486e4c0b1dfefaf72fb606dbd8
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 10:48:09 -0300

    Schema de Customer com o ZOD

---
+++ src/modules/customer/schemas/created-customer.schema.ts
@@ -0,0 +1,45 @@
+import { issue } from "node_modules/zod/v4/core/util.cjs";
+import z from "zod";
+
+
+export const CreatedCustomerSchema = z.object({
+    email: z.email("Invalid email format.")
+    .nonempty("Email Cannot be invalid."),
+
+    password: z.string({
+        error: (issue) =>
+            issue.input === undefined
+                ? 'Password cannot be invalid.'
+                : 'Must be a string.'
+    })
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .refine(value => !value.includes(' '), { message: "Password cannot contain spaces." })
+    .refine(value => /[#$%&*@!?\-"]/.test(value), { message: "Password must contain special characters." })
+    .refine(value => /\d/.test(value), { message: "Password must contain numbers." })
+    .refine(value => /[A-Z]/.test(value), { message: "Password must contain uppercase letters." })
+    .refine(value => /[a-z]/.test(value), { message: "Password must contain lowercase letters." })
+    .trim(),
+
+    name: z.string({
+        error: (issue) =>
+            issue.input === undefined
+                ? "Name cannot be invalid."
+                : "Must be a string."
+    }),
+
+    cpf: z.string({
+        error: (issue) =>
+            issue.input === undefined
+                ? "CPF cannot be invalid."
+                : "Must be a string."
+    })
+    .min(11)
+    .max(11),
+
+    phone: z.string({
+        error: (issue) =>
+            issue.input === undefined
+                ? "CPF cannot be invalid."
+                : "Must be a string."
+    })
+})
