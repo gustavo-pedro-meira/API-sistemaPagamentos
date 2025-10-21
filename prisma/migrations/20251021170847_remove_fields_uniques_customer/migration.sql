3807a1ee14088a18fb4807f5bd944bea5dfd8980
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-21 14:40:11 -0300

    Validação se existe CPF ou email

---
+++ prisma/migrations/20251021170847_remove_fields_uniques_customer/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "public"."Customer_cpf_key";
+
+-- DropIndex
+DROP INDEX "public"."Customer_email_key";
--- src/modules/customer/repositories/prisma/customer.prisma.repository.ts
+++ src/modules/customer/repositories/prisma/customer.prisma.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { CustomerRepository } from "../customer.repository";
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CustomerCreatedDto, CreatedCustomerDto } from "../../dto/created-customer.dto";
@@ -23,8 +23,24 @@ export class CustomerPrismaRepository implements CustomerRepository {
             where: { id }
         })
     }
+
     async create(createdCustomerDto: CreatedCustomerDto): Promise<CustomerCreatedDto | null> {
+        const verifyCustomerEmailExist = await this.prismaService.customer.findFirst({
+            where: { email: createdCustomerDto.email }
+        });
+        if(verifyCustomerEmailExist) {
+            throw new ConflictException("Email is already in use.")
+        }
+
+        const verifyCustomerCPFExist = await this.prismaService.customer.findFirst({
+            where: { cpf: createdCustomerDto.cpf }
+        });
+        if(verifyCustomerCPFExist) {
+            throw new ConflictException("CPF is already in use.")
+        }
+
         const passwordHashed = await hash(createdCustomerDto.password, 10);
+        
         return await this.prismaService.customer.create({
             data: { ...createdCustomerDto, password: passwordHashed }
         })
