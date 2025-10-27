171a0cf47224c73661f71e324b7b769e2c813fa9
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-10-27 10:01:52 -0300

    Configuração do KeyCloack no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,38 @@ import { CustomerModule } from './modules/customer/customer.module';
 import { PrismaService } from './infra/database/prisma.service';
 import { ChargeModule } from './modules/charge/charge.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { AuthGuard, KeycloakConnectModule, PolicyEnforcementMode, ResourceGuard, RoleGuard, TokenValidation } from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     CustomerModule,
     ChargeModule,
     ScheduleModule.forRoot(),
+    KeycloakConnectModule.register({
+      authServerUrl: "http://localhost:8080",
+      realm: "minha-api-pagamentos",
+      clientId: "api-pagamentos-client",
+      secret: "RmHKC5mlEZo12567MnGeHVGX97tK91G9",
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE,
+    })
   ],
   controllers: [],
   providers: [
     PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    }
   ],
 })
 export class AppModule {}
